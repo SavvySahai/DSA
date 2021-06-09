03a0787cd2be8b23ad1542cdc7e16c1b3fe12dd6
SavvySahai <savvysahai6843@gmail.com>
2021-06-09 12:03:23 +0530

    updated

--- Linked List/trial.cpp
+++ Linked List/trial.cpp
@@ -2,52 +2,85 @@
 using namespace std;
 
 class Node{
+    public:
     int data;
-    Node *next;     //next can have any name, basically to show tranversing with
+    Node *next;              //'next' can have any name, basically to show tranversing with
 
-    Node(){
-    this->data = data;      // since data is already mentioned so implictly mentioning to avoid confusion
-
+    Node(int data){
+    this->data = data;      // since data is already mentioned so implictly pointing to avoid confusion
+    next = nullptr;
     }
 };
 
 class LinkedList{
     Node *head;
     Node *tail;
+    public:
+    int size;
 
     LinkedList(){
         head = nullptr;
         tail = nullptr;
         size = 0;
     }
+
+
+    void push_end(int ele){
+        Node *n = new Node(ele);
+        if(tail==nullptr){
+            tail = head = n;
+            return;
+        }
+        
+        head->next=n;
+        
+        size++;
+    }
+    
+    void push_front(int ele){
+        Node *n = new Node(ele);
+        if(head==nullptr){
+            tail = head = n;
+        }
+        
+        tail->next=n;
+
+        size++;
+    }
+
+    void pop_end(){
+        if(tail==nullptr){
+            cout<<"List underflow"<<endl;
+            return;
+        }
+        if(tail==head){
+            tail= head = nullptr;
+            return;
+        } 
+        
+    
+
+        size--;
+        
+    }
+
+    void pop_front(){
+        if(head==nullptr){
+            cout<<"List underflow"<<endl;
+        }
+        if(head==tail){
+            head = tail = nullptr;
+            return;
+        }
+    }
+
+    //int view_last(){}
+
+    //int view_front(){}
+
+    //void dispaly_lst(){}
+
 };
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 int main(){
     LinkedList lst;
@@ -61,15 +94,15 @@ int main(){
     lst.push_front(50);
     lst.push_front(60);
 
-    cout<<"First element in list is "<<lst.view_front()<<endl;
-    cout<<"Last element in list is "<<lst.view_last()<<endl;
+    //cout<<"First element in list is "<<lst.view_front()<<endl;
+    //cout<<"Last element in list is "<<lst.view_last()<<endl;
 
-    lst.display_lst();
+    //lst.display_lst();
 
     lst.pop_end();
-    lst.pop_front();
+    //lst.pop_front();
 
-    lst.display_lst();
+    //lst.display_lst();
 
     return 0;
 }
