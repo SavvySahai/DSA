ddec9c8387bfe58c96c2f19d3bf9d5535ce84fd6
SavvySahai <savvysahai6843@gmail.com>
2021-06-24 00:24:34 +0530

    completed

--- Tree/TreeTraversal.cpp
+++ Tree/TreeTraversal.cpp
@@ -6,36 +6,57 @@ class Node{
     Node *left;
     Node *right;
 
-    Node(){
+    Node(int ele){                      //Don't forget to declare the values in the constructor
+        data = ele;
         left = right = nullptr;
     }
 };
 
+    void inorder(Node *root){           //At the end we need to print the values of root, so root needs to point to the data
+        if(root == nullptr) return;
+        inorder(root->left);
+        cout<<root->data<<" ";
+        inorder(root->right);
+    }
+
+    void preorder(Node *root){
+        if(root == nullptr) return;
+        cout<<root->data<<" ";
+        preorder(root->left);
+        preorder(root->right);
+    }
+
+    void postorder(Node *root){
+        if(root == nullptr) return;
+        postorder(root->left);
+        postorder(root->right);
+        cout<<root->data<<" ";
+    }
+
 int main(){
-    Node *root = new Node(1);
-    root->left = new Node(2);
+    Node *root = new Node(1);           //We can declare the root only once 
+    root->left = new Node(2);           //Then we can we the normal navigation method and 
     root->left->left = new Node(11);
 
-    Node *root  = new Node(1);
     root->right = new Node(3);
     root->right->left = new Node(4);
     root->right->left->left = new Node(10);
 
-    Node *root = new Node(3);
-    root->right = new Node(5);
-    root->right->left = new Node(6);
-    root->right->right->left = new Node(8);
-    
-    
+    root->right->right= new Node(5);
+    root->right->right->left = new Node(6);
+    root->right->right->left->left = new Node(8);
+    root->right->right->left->left->right = new Node(9);
 
+    root->right->right->right= new Node(7);
 
-    
+    cout<<"Preorder:  ";
+    preorder(root);
 
+    cout<<"Inorder: ";
+    inorder(root);
 
-
-
-
-    
+    cout<<"Postorder:  ";
+    postorder(root);
 
     return 0;
 }
