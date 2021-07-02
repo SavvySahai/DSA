04477bdeae30507e76e6977d614dffccca942865
SavvySahai <savvysahai6843@gmail.com>
2021-07-03 01:07:18 +0530

    updated the code

--- Tree/BinarySearchTree.cpp
+++ Tree/BinarySearchTree.cpp
@@ -47,10 +47,30 @@ class BST{
         if(ele <= root->data) return node_present(root->left,ele);
         else return node_present (root->right,ele);
         }
+    
+    //Insertion recur method 1
+    //Wrong method; Why?
 
-    // void insert(){
+    void insert_ele(Node *root, int ele){
+        if(root == nullptr){
+            root = new Node(ele);
+            return;
+        }
+        if (ele <= root->data){
+            if(root->left == nullptr){
+                root->left = new Node(ele);
+                }else insert_ele(root->left, ele);
+            }else{
+                if(root->right == nullptr){
+                    root->right = new Node(ele);
+                    }else insert_ele(root->right, ele);
+                }
+        }
 
-    // }
+    //Insertion recur method 2
+    //This is a right method
+        
+
 
     public:
     BST(){
@@ -59,34 +79,35 @@ class BST{
 
     //Iterative way of insertion (Iterative function means we use the while conditions)
     //It is different from recursive functions as in recursive functions, we call the same function by it's name.
+    //Also, we can call the iterative function in the public domain itself, unlike rrecursive, the data members can be aceesses without faultering with the program.
 
-    void insert_ele(int ele){
-        Node *n = new Node(ele);
-        Node *p = root;
+    // void insert_ele(int ele){
+    //     Node *n = new Node(ele);
+    //     Node *p = root;
 
-        if(p == nullptr){
-            root = n;
-            return;
-        }
+    //     if(p == nullptr){
+    //         root = n;
+    //         return;
+    //     }
 
-        while(p!= nullptr){
-            if(ele <= p->data){
-                if(p->left == nullptr){
-                    p->left = n;
-                    return;
-                }
-                p = p->left;
-            }else{ 
-                if(p->right == nullptr){
-                    p->right = n;
-                    return;
-                }
-                p = p->right;
-            }
-        }
+    //     while(p!= nullptr){
+    //         if(ele <= p->data){
+    //             if(p->left == nullptr){
+    //                 p->left = n;
+    //                 return;
+    //             }
+    //             p = p->left;
+    //         }else{ 
+    //             if(p->right == nullptr){
+    //                 p->right = n;
+    //                 return;
+    //             }
+    //             p = p->right;
+    //         }
+    //     }
 
 
-    }
+    // }
 
 
 
@@ -120,7 +141,10 @@ class BST{
         cout<<endl;
     }
 
-    //void insert_ele(int ele){}
+    //For insertion M1
+    // void insert(int ele){
+    //     insert_ele(root,ele);
+    // }
 
     void is_present(int ele){
         if(node_present (root,ele)){
@@ -138,7 +162,7 @@ int main(){
     vector<int> arr = {56,33,24,56,98,11,45,69,6,23,45,77,43};
 
     for(int ele : arr){
-        tree.insert_ele(ele);
+        tree.insert(ele);
     }
 
     tree.inorder();
